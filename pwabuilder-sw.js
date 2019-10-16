92e5bf4f2d96e1c4ceca082f144529774a03bab1
Gabriel Soares <37085637+GSoares10@users.noreply.github.com>
2019-10-16 11:01:42 -0300

    Add files via upload

---
+++ pwabuilder-sw.js
@@ -0,0 +1,60 @@
+// This is the "Offline page" service worker
+
+const CACHE = "pwabuilder-page";
+
+const offlineFallbackPage = "./offline.html";
+
+self.addEventListener("install", function (event) {
+  console.log("[PWA Builder] Install Event processing");
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Cached offline page during install");
+
+      if (offlineFallbackPage === "./offline.html") {
+        return cache.addAll([
+          './',
+          './index.html',
+          'offline.html',
+          './js/manifest.json',
+          './serviceWorker/pwabuilder-sw.js'
+        ]);
+      }
+
+      return cache.add(offlineFallbackPage);
+    })
+  );
+});
+
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request).catch(function (error) {
+
+      if (
+        event.request.destination !== "document" ||
+        event.request.mode !== "navigate"
+      ) {
+        return;
+      }
+
+      console.error("[PWA Builder] Network request Failed. Serving offline page " + error);
+      return caches.open(CACHE).then(function (cache) {
+        return cache.match(offlineFallbackPage);
+      });
+    })
+  );
+});
+
+self.addEventListener("refreshOffline", function () {
+  const offlinePageRequest = new Request(offlineFallbackPage);
+
+  return fetch(offlineFallbackPage).then(function (response) {
+    return caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Offline page updated from refreshOffline event: " + response.url);
+      return cache.put(offlinePageRequest, response);
+    });
+  });
+});
